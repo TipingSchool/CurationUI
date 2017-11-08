ec43241de7949345ab1cfb210270a0eeeacc718f
Suraj Prakash <surajprakash094@gmail.com>
2017-11-08 14:10:50 +0530

    published

---
+++ src/components/facets/App.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import {withRouter} from 'react-router-dom';
+
+
+
+class App extends Component {
+    constructor(props){
+        super(props);
+
+        console.log(this);
+    }
+   
+   findpublished =() => {
+
+    //    console.log(this)
+       this.props.history.push(this.props.location.pathname + '?find=published')
+   }
+
+    render(){
+        return(
+            <div>
+                <div onClick ={this.findpublished}> Published  </div>
+            </div>
+        )
+    }
+}
+
+
+export default withRouter(App);
